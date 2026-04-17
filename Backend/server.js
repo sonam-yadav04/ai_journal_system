9a7c51de5e487bd8fdc52a61a935589aa080ed02
sonam-yadav04 <sonamyadav.ml@gmail.com>
2026-04-17 14:27:22 +0530

    centrelized error hadeling is added

--- Backend/server.js
+++ Backend/server.js
@@ -3,20 +3,28 @@ import dotenv from 'dotenv';
 import DBConnect from './dbConfig/db.js';
 import morgan from 'morgan';
 import routes from './router/journalRoute.js';
+import authRoutes from './router/authRoutes.js';
+import { errorHandler } from './middleware/ErrorHandler.js';
 import cors from 'cors';
+
 dotenv.config();
 const app = express();
+
+DBConnect;
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-DBConnect;
+
 
 app.use('/api/journal', routes)
+app.use('/api/auth', authRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong" });
 });
+app.use(errorHandler);
 const port = 4000;
 
 app.listen(port,()=>{
