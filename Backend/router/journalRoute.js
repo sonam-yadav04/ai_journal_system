a00bafb5df342aa6ea22a5c76a434d681f28aba8
sonam-yadav04 <sonamyadav.ml@gmail.com>
2026-04-17 14:25:10 +0530

    new routes for trend , validatore are added

--- Backend/router/journalRoute.js
+++ Backend/router/journalRoute.js
@@ -1,19 +1,34 @@
 import express from 'express';
+import {auth} from "../middleware/auth.js";
+import { validateJournal } from "../validators/journalValidator.js";
+import validateRequest from "../middleware/validateRequest.js";
 import {
+  updateJournal,
+  delJournal, 
   createJournal,
   getJournals,
   analyzeText,
-  getInsights
+  getInsights,
+  getMoodTrend
 }  from "../controller/journalController.js";
 
+
 const router = express.Router();
-router.post("/", createJournal);
-router.get("/:userId", getJournals);
-router.post("/analyze", analyzeText);
-router.get("/insights/:userId", getInsights);
+router.post("/",auth,
+              validateJournal,
+              validateRequest,
+              createJournal);
+
+router.get("/", auth, getJournals);
+router.post("/analyze",analyzeText);
+router.get("/insights", auth ,getInsights);
+router.delete("/:id" ,auth, delJournal);
+router.put("/:id",auth,updateJournal);
+router.get("/trend/",auth,getMoodTrend);
+
 
 export default router;
-
+ 
 
 
 
